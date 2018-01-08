8ee767f99b6670887505605e82c1697997b8a7fa
gituserDoesNotExist <manfred-beutel@web.de>
2018-01-08 21:49:02 +0100

    jquery

--- public_html/index-new.js
+++ public_html/index-new.js
@@ -1,4 +1,4 @@
 var IndexWrapper = require('./index/IndexWrapper.js');
 
-indexWrapper = new IndexWrapper(document.getElementsByClassName('infobox')[0], $('info-sudoku'), $('info-tictactoe'), $('play-tictactoe'));
+indexWrapper = new IndexWrapper(document.getElementsByClassName('infobox')[0], jQuery('info-sudoku'), jQuery('info-tictactoe'), jQuery('play-tictactoe'));
 
