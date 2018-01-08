c33d19cef8c6774c447b28c6fcd6a1f8a2df529a
gituserDoesNotExist <manfred-beutel@web.de>
2018-01-08 22:08:48 +0100

    test und neue index.js

--- public_html/index-new.js
+++ public_html/index-new.js
@@ -1,12 +1,8 @@
 var IndexWrapper = require('./index/IndexWrapper.js');
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    document.getElementById('info-sudoku').addEventListener('click', function() {
-        console.log("hello");
-    });
-    
-    indexWrapper = new IndexWrapper(document.getElementsByClassName('infobox')[0], jQuery('info-sudoku'), jQuery('info-tictactoe'), jQuery('play-tictactoe'));
+       
+    indexWrapper = new IndexWrapper(document.getElementsByClassName('infobox')[0], jQuery('#info-sudoku'), jQuery('#info-tictactoe'), jQuery('#play-tictactoe'));
     indexWrapper.registerHandlers();
 
 }, false);
